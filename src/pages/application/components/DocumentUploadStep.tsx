1cb24fe24d0e3cec5f6246c96867ca2d1bf83f5e
Faisal-Sey <phaisalsey6@gmail.com>
2025-10-31 22:01:50 +0000

    document upload screen complete

--- src/pages/application/components/DocumentUploadStep.tsx
+++ src/pages/application/components/DocumentUploadStep.tsx
@@ -1,108 +1,213 @@
 import {FileUpload} from "@/components/ui/file-upload";
-import {useRef, useState} from "react";
+import {useRef, useState, FormEvent, useEffect} from "react";
 import {Input} from "@/components/ui/input";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 import {Checkbox} from "@/components/ui/checkbox";
 import {Button} from "@/components/ui/button";
+import {
+  useGetEligibilityVerificationQuery,
+  useSubmitDocumentUploadAndEligibilityVerificationMutation,
+  useUploadDocumentMutation
+} from "@/pages/application/api";
+import {DocumentTypes} from "@/pages/application/types";
 
 interface DocumentUploadStepProps {
   onBack?: () => void;
   onNext?: () => void;
 }
 
+interface FileUploadedInfo {
+  fileName: string;
+  fileUrl: string;
+  fileType: string;
+}
+
+const files = [
+  {
+    id: "photo-id",
+    label: "Photo ID (Driver’s License, State ID, Passport)",
+    placeholder: "Upload your photo ID"
+  },
+  {
+    id: "social-security-card",
+    label: "Social Security Card or valid work permit.",
+    placeholder: "Upload social security card"
+  },
+  {
+    id: "diploma",
+    label: "High School Diploma/GED certificate.",
+    placeholder: "Upload high school certificate"
+  },
+  {
+    id: "certifications",
+    label: "Any relevant certifications (e.g., CPR, First Aid — optional at this stage).",
+    placeholder: "Upload any certificate"
+  },
+  {
+    id: "hepatitis-b-vaccination",
+    label: "Hepatitis B vaccination series documents.",
+    placeholder: "Upload Hepatitis B vaccination series documents."
+  },
+  {
+    id: "hepatitis-b-immunity",
+    label: "Hepatitis B immunity (titer result)",
+    placeholder: "Upload Hepatitis B immunity (titer result)"
+  },
+  {
+    id: "tb-test",
+    label: "TB test result.",
+    placeholder: "Upload TB test result"
+  }
+]
+
 export default function DocumentUploadStep({onBack, onNext}: DocumentUploadStepProps) {
-  const [name, setName] = useState('');
-  const [selectedFileName, setSelectedFileName] = useState<string | null>(null);
-  const [uploadError, setUploadError] = useState<string | null>(null);
+  const [documentTypeUploading, setDocumentTypeUploading] = useState<DocumentTypes | null>(null);
   const ref = useRef<HTMLInputElement>(null);
   const [value, setValue] = useState(false);
+  const [fileUploads, setFileUploads] = useState<FileUploadedInfo[]>([]);
 
-  const onBlur = () => {
-    if (!selectedFileName) {
-      setUploadError('Please select a file');
-    }
-  };
+  const [uploadFile, {isLoading}] = useUploadDocumentMutation();
+  const {data: eligibilityVerificationData} = useGetEligibilityVerificationQuery(undefined, {
+    refetchOnMountOrArgChange: true
+  });
 
-  const onChange = (files: FileList | null) => {
-    if (files && files.length > 0) {
-      setSelectedFileName(files[0].name);
-    }
-  };
+  const [
+    submitDocumentUploadAndEligibilityVerification,
+    {isLoading: isSubmitting}
+  ] = useSubmitDocumentUploadAndEligibilityVerificationMutation();
 
   const [references, setReferences] = useState<{
     name: string;
     relationship: string;
-    phone: string;
+    phoneNumber: string;
     email: string;
   }[]>([
     {
       name: "",
       relationship: "",
-      phone: "",
+      phoneNumber: "",
       email: ""
     },
     {
       name: "",
       relationship: "",
-      phone: "",
+      phoneNumber: "",
       email: ""
     }
   ])
 
-  const files = [
-    {
-      id: "photo-id",
-      label: "Photo ID (Driver’s License, State ID, Passport)",
-      placeholder: "Upload your photo ID"
-    },
-    {
-      id: "social-security",
-      label: "Social Security Card or valid work permit.",
-      placeholder: "Upload social security card"
-    },
-    {
-      id: "high-school",
-      label: "High School Diploma/GED certificate.",
-      placeholder: "Upload high school certificate"
-    },
-    {
-      id: "relevant-certificate",
-      label: "Any relevant certifications (e.g., CPR, First Aid — optional at this stage).",
-      placeholder: "Upload any certificate"
-    },
-    {
-      id: "hepatitis-b-vaccination",
-      label: "Hepatitis B vaccination series documents.",
-      placeholder: "Upload Hepatitis B vaccination series documents."
-    },
-    {
-      id: "hepatitis-b-immunity",
-      label: "Hepatitis B immunity (titer result)",
-      placeholder: "Upload Hepatitis B immunity (titer result)"
-    },
-    {
-      id: "tb-test",
-      label: "TB test result.",
-      placeholder: "Upload TB test result"
+  const handleFileUpload = async (
+    files: FileList | null,
+    documentType: DocumentTypes
+  ) => {
+    try {
+      if (!files) return;
+      const file = files[0];
+      const formData = new FormData();
+      formData.append("file", file);
+      setDocumentTypeUploading(documentType);
+      const response = await uploadFile({
+        data: formData,
+        documentType
+      }).unwrap();
+
+      setFileUploads((prev) => [
+        ...prev.filter((item) => item.fileType !== documentType),
+        {
+          fileName: response.data.fileName,
+          fileUrl: response.data.url,
+          fileType: documentType
+        }
+      ]);
+    } catch (error) {
+      console.error("Error uploading file:", error);
     }
-  ]
+  };
+
+  const handleSubmit = async (
+    event: FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    try {
+      const response = await submitDocumentUploadAndEligibilityVerification({
+        data: {
+          photoIdUrl: fileUploads.find((item) => item.fileType === "photo-id")?.fileUrl,
+          socialSecurityCardUrl: fileUploads.find((item) => item.fileType === "social-security-card")?.fileUrl,
+          diplomaUrl: fileUploads.find((item) => item.fileType === "diploma")?.fileUrl,
+          certificationsUrl: fileUploads.find((item) => item.fileType === "certifications")?.fileUrl,
+          hepatitisBVaccinationUrl: fileUploads.find((item) => item.fileType === "hepatitis-b-vaccination")?.fileUrl,
+          hepatitisBImmunityUrl: fileUploads.find((item) => item.fileType === "hepatitis-b-immunity")?.fileUrl,
+          tbTestResultUrl: fileUploads.find((item) => item.fileType === "tb-test")?.fileUrl,
+          i9FormUrl: fileUploads.find((item) => item.fileType === "i9-form")?.fileUrl,
+          w4FormUrl: fileUploads.find((item) => item.fileType === "w4-form")?.fileUrl,
+          references,
+          declarationAgreed: value
+        },
+        method: eligibilityVerificationData ? "PUT" : "POST"
+      }).unwrap();
+      console.log(response);
+      onNext?.();
+    } catch (error) {
+      console.error("Error submitting document upload and eligibility verification:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (eligibilityVerificationData) {
+      setReferences(eligibilityVerificationData.data.references);
+      setValue(eligibilityVerificationData.data.declarationAgreed);
+      const fileKeys = [
+        "photoIdUrl",
+        "socialSecurityCardUrl",
+        "diplomaUrl",
+        "certificationsUrl",
+        "hepatitisBVaccinationUrl",
+        "hepatitisBImmunityUrl",
+        "tbTestResultUrl",
+        "i9FormUrl",
+        "w4FormUrl"
+      ]
+      const fileKeysIds = {
+        "photoIdUrl": "photo-id",
+        "socialSecurityCardUrl": "social-security-card",
+        "diplomaUrl": "diploma",
+        "certificationsUrl": "certifications",
+        "hepatitisBVaccinationUrl": "hepatitis-b-vaccination",
+        "hepatitisBImmunityUrl": "hepatitis-b-immunity",
+        "tbTestResultUrl": "tb-test",
+        "i9FormUrl": "i9-form",
+        "w4FormUrl": "w4-form"
+      }
+      setFileUploads(Object.entries(eligibilityVerificationData.data).filter(([key, value]) => fileKeys.includes(key) && value).map(([key, value]) => {
+        const splittedFileUrl = value ? value.split("/") : []
+        return {
+          fileName: splittedFileUrl[splittedFileUrl.length - 1],
+          fileUrl: value,
+          fileType: fileKeysIds[key as keyof typeof fileKeysIds] as DocumentTypes
+        }
+      }))
+    }
+  }, [eligibilityVerificationData]);
+
+  useEffect(() => {
+    console.log(references)
+  }, [references]);
 
   return (
-    <div className={"w-full"}>
+    <form className={"w-full"} onSubmit={handleSubmit}>
       {files.map((file) =>
         <div className={"mb-6"}>
           <p className={"text-sm mb-2"}>{file.label}</p>
           <FileUpload
             name={file.id}
             className="h-[90px] w-full max-w-[100vw]"
-            label={selectedFileName ?? file.placeholder}
-            accept=".pdf,.doc,.docx"
-            onBlur={onBlur}
-            onChange={(event) => {
-              onChange(event.target.files ?? null);
-              setUploadError(null);
+            label={(isLoading && file.id === documentTypeUploading) ? "Uploading..." : file.placeholder}
+            accept=".pdf, .jpg, .png, .webp"
+            onChange={async (event) => {
+              await handleFileUpload(event.target.files ?? null, file.id as DocumentTypes);
             }}
           />
+          <FileNameCard fileUploads={fileUploads} documentType={file.id as DocumentTypes}/>
         </div>
       )}
       <div className={"mb-6"}>
@@ -124,14 +229,13 @@ export default function DocumentUploadStep({onBack, onNext}: DocumentUploadStepP
         <FileUpload
           name={"upload-i-9-form"}
           className="h-[90px] w-full max-w-[100vw]"
-          label={selectedFileName ?? "Upload I-9 form"}
-          accept=".pdf,.doc,.docx"
-          onBlur={onBlur}
-          onChange={(event) => {
-            onChange(event.target.files ?? null);
-            setUploadError(null);
+          label={(isLoading && "i9-form" === documentTypeUploading) ? "Uploading..." : "i9-form"}
+          accept=".pdf, .jpg, .png, .webp"
+          onChange={async (event) => {
+            await handleFileUpload(event.target.files ?? null, "i9-form");
           }}
         />
+        <FileNameCard fileUploads={fileUploads} documentType={"i9-form" as DocumentTypes}/>
       </div>
       <div className={"mb-6"}>
         <p className={"text-sm mb-2 flex items-center"}>
@@ -152,14 +256,13 @@ export default function DocumentUploadStep({onBack, onNext}: DocumentUploadStepP
         <FileUpload
           name={"upload-w-4-form"}
           className="h-[90px] w-full max-w-[100vw]"
-          label={selectedFileName ?? "Upload W-4 Form"}
-          accept=".pdf,.doc,.docx"
-          onBlur={onBlur}
-          onChange={(event) => {
-            onChange(event.target.files ?? null);
-            setUploadError(null);
+          label={(isLoading && "w4-form" === documentTypeUploading) ? "Uploading..." : "w4-form"}
+          accept=".pdf, .jpg, .png, .webp"
+          onChange={async (event) => {
+            await handleFileUpload(event.target.files ?? null, "w4-form");
           }}
         />
+        <FileNameCard fileUploads={fileUploads} documentType={"w4-form" as DocumentTypes}/>
       </div>
 
       <div>
@@ -174,11 +277,31 @@ export default function DocumentUploadStep({onBack, onNext}: DocumentUploadStepP
                   type="text"
                   placeholder={"Enter name"}
                   className="w-full pr-10"
+                  value={reference.name}
+                  onChange={(e) => setReferences((prev) => {
+                    const newReferences = [...prev];
+                    newReferences[index] = {
+                      ...newReferences[index],
+                      name: e.target.value
+                    };
+                    return newReferences;
+                  })}
+                  required
                 />
               </div>
               <div>
                 <label className={"text-sm mb-2"}>Relationship</label>
-                <Select>
+                <Select
+                  required
+                  value={reference.relationship}
+                  onValueChange={(value) => setReferences((prev) => {
+                    const newReferences = [...prev];
+                    newReferences[index] = {
+                      ...newReferences[index],
+                      relationship: value || reference.relationship
+                    };
+                    return newReferences;
+                  })}>
                   <SelectTrigger className={"w-full"}>
                     <SelectValue placeholder="Select Relationship"/>
                   </SelectTrigger>
@@ -194,9 +317,19 @@ export default function DocumentUploadStep({onBack, onNext}: DocumentUploadStepP
               <div>
                 <label className={"text-sm mb-2"}>Phone Number</label>
                 <Input
+                  required
                   type="text"
                   placeholder={"Enter phone number"}
                   className="w-full pr-10"
+                  value={reference.phoneNumber}
+                  onChange={(e) => setReferences((prev) => {
+                    const newReferences = [...prev];
+                    newReferences[index] = {
+                      ...newReferences[index],
+                      phoneNumber: e.target.value
+                    };
+                    return newReferences;
+                  })}
                 />
               </div>
               <div>
@@ -205,6 +338,15 @@ export default function DocumentUploadStep({onBack, onNext}: DocumentUploadStepP
                   type="email"
                   placeholder={"Enter email"}
                   className="w-full pr-10"
+                  value={reference.email}
+                  onChange={(e) => setReferences((prev) => {
+                    const newReferences = [...prev];
+                    newReferences[index] = {
+                      ...newReferences[index],
+                      email: e.target.value
+                    };
+                    return newReferences;
+                  })}
                 />
               </div>
             </div>
@@ -215,8 +357,7 @@ export default function DocumentUploadStep({onBack, onNext}: DocumentUploadStepP
         <Checkbox
           className={"border-[#808081] bg-transparent w-4 h-4 mr-2"}
           ref={ref}
-          name={name}
-          onBlur={onBlur}
+          name={"declare"}
           checked={value}
           onChange={(event) => setValue(event.target.checked)}
           labelClassName={"text-[#808081] font-normal"}
@@ -225,12 +366,12 @@ export default function DocumentUploadStep({onBack, onNext}: DocumentUploadStepP
       </div>
       <div className="mt-6 pb-6">
         <Button
-          type="button"
-          onClick={onNext}
+          type="submit"
+          disabled={isSubmitting}
           className={'bg-[#00B4B8] backdrop-blur-[22px] hover:bg-[#00B4B8] active:bg-[#b2b2b3]'}
         >
             <span>
-              {'Next'}
+              {isSubmitting ? 'Submitting' : 'Next'}
             </span>
           <svg className="h-5 w-5" viewBox="0 0 20 20" fill="none">
             <path
@@ -244,7 +385,32 @@ export default function DocumentUploadStep({onBack, onNext}: DocumentUploadStepP
         </Button>
       </div>
 
-    </div>
+    </form>
   );
 }
 
+const FileNameCard = (
+  {fileUploads, documentType}: {
+    fileUploads: FileUploadedInfo[];
+    documentType: DocumentTypes;
+  }
+) => {
+  const fileUploaded = fileUploads.find((item) => item.fileType === documentType);
+  if (!fileUploaded) return null;
+  return (
+    <div className={"flex space-x-2 my-3 bg-[#00D84114] p-3 rounded"}>
+      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path opacity="0.4" d="M6.66675 5.83301H13.3334" stroke="#00944A" stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"/>
+        <path opacity="0.4" d="M6.66675 9.16699H10.0001" stroke="#00944A" stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"/>
+        <path
+          d="M10.8333 17.917V17.5003C10.8333 15.1433 10.8333 13.9648 11.5655 13.2326C12.2978 12.5003 13.4763 12.5003 15.8333 12.5003H16.2499M16.6666 11.1196V8.33366C16.6666 5.19096 16.6666 3.61962 15.6903 2.6433C14.714 1.66699 13.1426 1.66699 9.99992 1.66699C6.85723 1.66699 5.28588 1.66699 4.30956 2.6433C3.33325 3.61961 3.33325 5.19096 3.33325 8.33366V12.1205C3.33325 14.8247 3.33325 16.1767 4.07164 17.0926C4.22082 17.2776 4.38934 17.4461 4.57436 17.5952C5.49018 18.3337 6.84227 18.3337 9.54642 18.3337C10.1344 18.3337 10.4283 18.3337 10.6976 18.2387C10.7536 18.2189 10.8084 18.1962 10.862 18.1706C11.1196 18.0473 11.3274 17.8395 11.7432 17.4237L15.6903 13.4767C16.172 12.9949 16.4128 12.7541 16.5398 12.4477C16.6666 12.1415 16.6666 11.8008 16.6666 11.1196Z"
+          stroke="#00944A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      <span>{fileUploaded?.fileName}</span>
+    </div>
+  )
+}
